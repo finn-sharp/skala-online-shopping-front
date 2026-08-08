9e4f11b1fe77bc10ca17b89e1bff3025b06ede2e
JHuiJung <wjdgmlwnd12@naver.com>
2026-08-08 11:59:27 +0900

    feat/[Feat] 홈,로그인, 회원가입 테스트 버튼 추가
    - App.jsx의 Vite 기본 템플릿 제거
    - App.jsx에서 3가지 페이지로 이동할 수 있는 라우터 추가
    - 홈, 로그인, 회원가입 페이지에 테스트용 버튼 추가
    
    #4

--- skala-front/src/pages/Home.jsx
+++ skala-front/src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+
 function Home() {
-  return <h1>홈</h1>
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <h1>홈</h1>
+      <button type="button" onClick={() => navigate('/login')}>
+        로그인
+      </button>
+      <button type="button" onClick={() => navigate('/signup')}>
+        회원가입
+      </button>
+    </>
+  )
 }
 
 export default Home
--- skala-front/src/pages/Login.jsx
+++ skala-front/src/pages/Login.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+
 function Login() {
-  return <h1>로그인</h1>
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <h1>로그인</h1>
+      <button type="button" onClick={() => navigate('/signup')}>
+        회원가입
+      </button>
+      <button type="button" onClick={() => navigate('/')}>
+        홈
+      </button>
+    </>
+  )
 }
 
 export default Login
